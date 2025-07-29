68f7529df7dceb358cd1ec233bc50daa0d342077
KiranWelisa <kiran.knoppert@welisa.com>
2025-07-29 10:32:33 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    // The API proxy URL will automatically use the correct domain
-    NEXT_PUBLIC_API_BASE_URL: process.env.VERCEL_URL 
-      ? `https://${process.env.VERCEL_URL}/api/proxy`
-      : 'http://localhost:3000/api/proxy'
-  }
 }
 
 module.exports = nextConfig
-
-// Trigger deployment
