7937f870eb8ba4472a6aa128b881eae62acbc10c
KiranWelisa <kiran.knoppert@welisa.com>
2025-09-03 14:14:42 +0000

    auth bugfix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,14 +4,13 @@ import GoogleProvider from 'next-auth/providers/google';
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
-          // Hier vragen we om de benodigde permissies voor Google Drive.
           scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive",
         },
       },
@@ -19,18 +18,18 @@ export default NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    // Deze callback wordt aangeroepen bij het aanmaken of bijwerken van een JSON Web Token.
-    // We slaan hier het accessToken van Google op in de token.
     async jwt({ token, account }) {
+      // Persist the OAuth access_token and refresh_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
       }
       return token;
     },
-    // Deze callback wordt aangeroepen wanneer een sessie wordt opgevraagd door de client.
-    // We voegen het accessToken toe aan het sessie-object, zodat het in de frontend beschikbaar is.
     async session({ session, token }) {
+      // Send properties to the client, like an access_token and user id from a provider.
       session.accessToken = token.accessToken;
+      session.refreshToken = token.refreshToken;
       return session;
     },
   },
